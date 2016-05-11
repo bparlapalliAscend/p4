e5d4f72c33610e5be07d3fd2084621092f1ee42f
bparlapalliAscend <bharath.parlapalli@gmail.com>
2016-05-11 18:43:45 -0400

    fix html error div not closed on index page, changed the location of jquery css from footer area to header in master layout view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -56,7 +56,7 @@
          @yield('content')
     </section>
 
-   	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 		
 
   		  {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
